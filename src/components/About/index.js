3a9aa9ec2e8552f2c84e27a8749f967ff21f520a
bala566 <baladurga566@gmail.com>
2022-11-01 10:44:21 +0000

    authentication functionality

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+import LogoutButton from '../LogoutButton'
+import Header from '../Header'
+import './index.css'
+
+const About = () => (
+  <>
+    <Header />
+    <div className="about-bg-container">
+      <h1>About Route</h1>
+    </div>
+    <LogoutButton />
+  </>
+)
+
+export default About
